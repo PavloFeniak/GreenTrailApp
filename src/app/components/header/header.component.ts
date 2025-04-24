14d4b279d5a5db55d52a55f3e3c94bd71b447b56
PavloFeniak <phenyakpablo@gmail.com>
2025-04-24 22:55:23 +0300

    created header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NgIf
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
