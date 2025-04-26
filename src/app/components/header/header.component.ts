d617ced4a04b944e301492066e73e5c226af1c54
PavloFeniak <phenyakpablo@gmail.com>
2025-04-27 02:55:37 +0300

    home page edit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,66 @@
-import { Component } from '@angular/core';
-import {NgIf} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {NgClass, NgIf} from '@angular/common';
+import * as ScrollMagic from 'scrollmagic';
+import { gsap } from 'gsap';
+import {Scene} from 'scrollmagic';
+
 
 @Component({
   selector: 'app-header',
   imports: [
-    NgIf
+    NgIf,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  private controller: ScrollMagic.Controller;
+  public updated = false;
+  private timeoutId: any;
+
+
+  constructor() {
+    this.controller = new ScrollMagic.Controller();
+  }
+
+  ngOnInit() {
+    // @ts-ignore
+    const scene = new ScrollMagic.Scene({
+      triggerHook: 0,
+      offset: 100 // на 100px від верху
+    })
+      .on('enter', () => {
+        gsap.to('#header', { opacity: 0, duration: 0.3 });
+
+        // Скасовуємо попередній таймер (на всяк випадок)
+        if (this.timeoutId) {
+          clearTimeout(this.timeoutId);
+        }
+
+        // Ставимо новий таймер
+        this.timeoutId = setTimeout(() => {
+          this.updated = true;
+          gsap.to('#header', { opacity: 1, duration: 0.5 });
+        }, 1000);
+      })
+      .on("enter", (event: any) => {  // <--- перехоплюємо тип any
+        if (event.scrollDirection === 'REVERSE') {
+          // Якщо скролимо назад вгору
+          gsap.to('#header', { opacity: 0, duration: 1 });
+
+          if (this.timeoutId) {
+            clearTimeout(this.timeoutId); // скасувати таймер оновлення, якщо ще не оновили
+          }
+
+          this.updated = false; // повернути дефолтний стан
+          setTimeout(() => {
+            gsap.to('#header', { opacity: 1, duration: 1 });
+          }, 50); // легка затримка для плавності
+        }
+      })
+      .addIndicators({ name: "Header Trigger" }) // видно на екрані де тригер
+      .addTo(this.controller);
+  }
 }
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import $ from 'jquery';
-import 'animate.css'
+import 'animate.css';
+import * as L from 'leaflet';
 
+declare var SMap: any;
 
 @Component({
   selector: 'app-home-page',
@@ -9,13 +11,45 @@ import 'animate.css'
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit  {
+  private map: L.Map | undefined;
+  private readonly API_KEY = 'HFo20yrq4X8ulf_mMAW3PcqNNtPA5qrudwayM8ZM4NI';
+
+
   ngOnInit(): void {
     $(document).ready(() => {
       this.initParallaxImages();
     });
   }
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+  private initMap(): void {
+    this.map = L.map('map').setView([48.6260542, 23.9013792], 16);
 
+    L.tileLayer(`https://api.mapy.cz/v1/maptiles/outdoor/256/{z}/{x}/{y}?apikey=${this.API_KEY}`, {
+      minZoom: 0,
+      maxZoom: 19,
+      attribution: '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+    }).addTo(this.map);
+
+    const LogoControl = L.Control.extend({
+      options: {
+        position: 'bottomleft',
+      },
+      onAdd: function () {
+        const container = L.DomUtil.create('div');
+        const link = L.DomUtil.create('a', '', container);
+        link.setAttribute('href', 'http://mapy.cz/');
+        link.setAttribute('target', '_blank');
+        link.innerHTML = '<img src="https://api.mapy.cz/img/api/logo.svg" alt="Mapy.cz Logo"/>';
+        L.DomEvent.disableClickPropagation(link);
+        return container;
+      }
+    });
+
+    this.map.addControl(new LogoControl());
+  }
   initParallaxImages(): void {
     $('[data-parallax="scroll"]').each(function () {
       const $el = $(this);
