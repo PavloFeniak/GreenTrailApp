77595d278dbcf4876089dbec9a163b761adf135d
PavloFeniak <phenyakpablo@gmail.com>
2025-05-04 00:27:31 +0300

    add reusable track preview component

---
+++ src/app/components/trek-preview/trek-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trek-preview',
+  imports: [],
+  templateUrl: './trek-preview.component.html',
+  styleUrl: './trek-preview.component.css'
+})
+export class TrekPreviewComponent {
+
+}
