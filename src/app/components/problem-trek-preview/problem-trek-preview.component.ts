a665139ea67bcafc4af5c1f6ab691adb0c44009e
PavloFeniak <phenyakpablo@gmail.com>
2025-05-05 01:35:48 +0300

    Create reusable component for preview

---
+++ src/app/components/problem-trek-preview/problem-trek-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem-trek-preview',
+  imports: [],
+  templateUrl: './problem-trek-preview.component.html',
+  styleUrl: './problem-trek-preview.component.css'
+})
+export class ProblemTrekPreviewComponent {
+
+}
