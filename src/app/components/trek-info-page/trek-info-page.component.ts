3cc36030e5c18070de3485e5a360be46f6c94912
PavloFeniak <phenyakpablo@gmail.com>
2025-05-06 22:14:13 +0300

    create page to view info about trek

---
+++ src/app/components/trek-info-page/trek-info-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trek-info-page',
+  imports: [],
+  templateUrl: './trek-info-page.component.html',
+  styleUrl: './trek-info-page.component.css'
+})
+export class TrekInfoPageComponent {
+
+}
